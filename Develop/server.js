02098f5d60cae12ab923287da3880c8b341a717d
Tate <tatesutter04@gmail.com>
2025-01-11 03:12:55 -0600

    updated server file

--- Develop/server.js
+++ Develop/server.js
@@ -1,1 +1,12 @@
-const port = process.env.PORT || 3001
+import express from 'express';
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
